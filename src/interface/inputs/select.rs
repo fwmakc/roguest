ecfea8df930b5a771a0659ec61615109b2f6fd66
fwmakc <fwmakcpro@yandex.ru>
2026-01-18 02:46:45 +0300

    fix selector

--- src/interface/inputs/select.rs
+++ src/interface/inputs/select.rs
@@ -5,14 +5,6 @@ use dialoguer::{Select, theme::ColorfulTheme};
 pub fn select(items: Vec<&str>) -> usize {
     println!("");
 
-    let items = vec![
-        "Осмотреться",
-        "Пойти в таверну",
-        "Отправиться в лес",
-        "Посмотреть инвентарь",
-        "Выйти",
-    ];
-
     Select::with_theme(&ColorfulTheme::default())
         .items(&items)
         .default(0)
