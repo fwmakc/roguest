8102cac206dedcf51991d8ba9ecc0d1c9b44c9c5
fwmakc <fwmakcpro@yandex.ru>
2026-01-18 03:05:25 +0300

    fix wait and input with empty user string

--- src/interface/inputs/input.rs
+++ src/interface/inputs/input.rs
@@ -1,10 +1,20 @@
 use std::io::{self};
 
+use crate::interface::prints;
+
 pub fn input(label: &str) -> String {
-    let mut input = String::new();
-    println!("{}", label);
+    loop {
+        let mut input = String::new();
+        println!("{}", label);
 
-    io::stdin().read_line(&mut input).expect("Ошибка чтения");
+        io::stdin().read_line(&mut input).expect("Ошибка чтения");
 
-    input.trim().to_string()
+        let trimmed = input.trim();
+
+        if !trimmed.is_empty() {
+            return trimmed.to_string();
+        }
+
+        prints::error("Ошибка: ввод не может быть пустым. Пожалуйста, введите текст.");
+    }
 }
--- src/interface/prints/wait_any_key.rs
+++ src/interface/prints/wait_any_key.rs
@@ -1,8 +1,12 @@
+use std::io;
+
 use colored::Colorize;
 
 use crate::interface::inputs;
 
 pub fn wait_any_key() {
-    print!("\n{}", "Нажмите любую клавишу, чтобы продолжить".blue());
-    inputs::input("");
+    let mut input = String::new();
+    println!("{}", "Нажмите любую клавишу, чтобы продолжить".blue());
+
+    io::stdin().read_line(&mut input).expect("Ошибка чтения");
 }
